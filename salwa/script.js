2488b6c4e97ce2b6ecae6b0eef6a2384a44a9b3f
Adnan F. Muhammad <kickadnan@gmail.com>
2020-07-02 22:57:56 +0700

    new

--- salwa/script.js
+++ salwa/script.js
@@ -1,3 +1,4 @@
-// I am going to continue to improve this...
-// I attempt to perform candle again but could not overcome the previous version.
-// https://codepen.io/fixcl/pen/nKFDr
+/*
+Background Gradients From -- 
+https://uigradients.com
+*/
