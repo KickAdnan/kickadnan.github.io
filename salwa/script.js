b7a45b2ebc51c443ed3ba61951270256e8cbfca1
Adnan F. Muhammad <kickadnan@gmail.com>
2020-07-02 23:47:32 +0700

    add function

--- salwa/script.js
+++ salwa/script.js
@@ -2,3 +2,20 @@
 Background Gradients From -- 
 https://uigradients.com
 */
+
+var myDate = new Date();
+    var hrs = myDate.getHours();
+
+    var greet;
+
+    if (hrs < 10)
+        greet = 'Good Morning Sunshine!';
+    else if (hrs >= 10 && hrs <= 17)
+        greet = 'Hello Salwa!';
+    else if (hrs >= 17 && hrs <= 21)
+        greet = 'Hai Yeoppo!';
+    else if (hrs >= 21 && hrs <= 24)
+        greet = 'Goodnight Sweetheath!';
+
+    document.getElementById('lblGreetings').innerHTML =
+         greet ;
