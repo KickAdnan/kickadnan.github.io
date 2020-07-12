958f8ac18cf0d65c5f80c2dfaa81c42d2e8a2777
Adnan F. Muhammad <kickadnan@gmail.com>
2020-07-12 23:52:09 +0700

    The moon is beautiful, isn’t it?

--- salwa/script.js
+++ salwa/script.js
@@ -8,14 +8,11 @@ var myDate = new Date();
 
     var greet;
 
-    if (hrs < 10)
+    if (hrs < 8)
         greet = 'Good Morning Sunshine!';
-    else if (hrs >= 10 && hrs <= 17)
-        greet = 'Hello Salwa!';
-    else if (hrs >= 17 && hrs <= 21)
-        greet = 'Hai Yeoppo!';
-    else if (hrs >= 21 && hrs <= 24)
-        greet = 'Goodnight Sweethearth!';
+    else if (hrs >= 8 && hrs <= 24)
+        greet = 'The moon is beautiful, isn’t it?';
+
 
     document.getElementById('lblGreetings').innerHTML =
          greet ;
